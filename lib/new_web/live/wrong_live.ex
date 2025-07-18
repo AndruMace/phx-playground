eb921b4a47dc82b46582536807b7b4c80618caf8
AndruMace <amacedeveloper@gmail.com>
2025-07-18 17:11:52 -0500

    Ch.1 exercises

---
+++ lib/new_web/live/wrong_live.ex
@@ -0,0 +1,49 @@
+defmodule NewWeb.WrongLive do
+  use NewWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time())}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1 class="mb-4 text-4xl font-extrabold">Your Score: {@score}</h1>
+    <h2>
+      {@message}
+      It's {@time}
+    </h2>
+    <br/>
+    <h2>
+      <%= for n <- 1..10 do %>
+        <.link
+          class="bg-blue-500 hover:bg-blue-700 text-white
+          font-bold py-2 px-4 bordewr-blue-700 rounded m-1"
+          phx-click="guess"
+          phx-value-number={n}>
+
+          {n}
+        </.link>
+      <% end %>
+    </h2>
+    """
+  end
+
+  def time() do
+    DateTime.utc_now |> to_string
+  end
+
+  def handle_event("guess", %{"number" => guess}, socket) do
+    message = "Your guess: #{guess}. Wrong. Guess again."
+    score = socket.assigns.score - 1
+    time = time()
+    {
+      :noreply,
+      assign(
+        socket,
+        message: message,
+        score: score,
+        time: time
+      )
+    }
+  end
+end
